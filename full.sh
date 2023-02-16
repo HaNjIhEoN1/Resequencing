a013f8b9a30891b16b8c3af656c3e5554dada7df
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-02-16 14:16:23 +0900

    Update full.sh

--- full.sh
+++ full.sh
@@ -10,12 +10,12 @@ bcftools mpileup -g 5 -Oz -o gvcf/{}.gvcf.gz -f assembly/GCF_001433935.1_IRGSP-1
 
 bcftools call -g 5 -m -Oz -o gvcf/{}.call.gvcf.gz gvcf/{}.gvcf.gz :::: srrlist2.txt &&
 
-bcftools index ./output/{}.call.gvcf.gz :::: srrlist2.txt
-
 bcftools norm -f /data/data/assembly/GCF_001433935.1_IRGSP-1.0_genomic.fna -Oz -o /data/data/output/{}.norm.gvcf.gz /data/data/output/{}.call.gvcf.gz :::: srrlist2.txt
+
+bcftools index ./output/{}.norm.gvcf.gz :::: srrlist2.txt
 
 echo "finish"
 
 bcftools merge -g /data/data/assembly/GCF_001433935.1_IRGSP-1.0_genomic.fna -m both -Oz -o /data/data/norm/test_merged.vcf.gz /data/data/norm/*.norm.gvcf.gz
 
-bcftools view -i 'F_MISSING < 0.05 && MAF > 0.1 && N_ALT>0 && %QUAL>= 30 && MQ >= 20' -Oz -o /data/data/output/test_merged.i.vcf.gz /data/data/output/test_merged.vcf.gz
+bcftools view -i 'F_MISSING < 0.25 && MAF > 0.05 && N_ALT>0 && %QUAL>= 30 && MQ >= 20' -Oz -o /data/data/output/test_merged.i.vcf.gz /data/data/output/test_merged.vcf.gz
