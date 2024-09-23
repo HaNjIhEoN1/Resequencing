95c99b66aaae9038e34eb6f5d937ec0bb87fdb3d
Jiheon <122258679+HaNjIhEoN1@users.noreply.github.com>
2024-09-23 22:51:51 +0900

    Update 2.varinatcalling.sh

--- 2.varinatcalling.sh
+++ 2.varinatcalling.sh
@@ -1,7 +1,7 @@
 # 실습에 사용된 organism은 rice(Oryza. sativa. L)이며 ref는 NCBI에서 download. 또한, download 받을 fastq는 krice core v3에서 선택
 
 # varinats calling by using bcftools
-bcftools mpileup –g 5 -Oz –o /data/data/SRR12701911/gvcf/SRR12701911.gvcf.gz –f /data/data/assembly/GCF_001433935.1_IRGSP-1.0_genomic.fna  /data/data/SRR12701911/sortBam/SRR12701911.sort.bam
+bcftools mpileup –g 5 -Oz –o /data/data/SRR12701911/gvcf/SRR12701911.gvcf.gz –f /data/data/assembly/GCF_001433935.1_IRGSP-1.0_genomic.fna  /data/data/SRR12701911/sortBam/SRR12701911.markdup.bam
 
 bcftools call -g 5 -m -Oz -o gvcf/SRR12701911.call.gvcf.gz gvcf/SRR12701911.gvcf.gz
 
